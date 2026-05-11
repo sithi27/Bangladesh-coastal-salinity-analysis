44ed2131c44a84277734b08699b4fcb0f0c424e2
sithi27 <fariaafrinsithi@gmail.com>
2026-05-11 12:35:48 +0600

    Add files via upload

---
+++ Python/python 1.py
@@ -0,0 +1,79 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+import pandas as pd
+import pymysql
+import matplotlib.pyplot as plt
+
+print("Start")
+
+try:
+    conn = pymysql.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+    print("Connected")
+
+    query = "SELECT * FROM salinity_data"
+    df = pd.read_sql(query, conn)
+
+    print("Total rows:", len(df))
+    print(df.head())
+    print(df["District"].unique())
+    print(df.info())
+    print(df.columns)
+    
+    district_count = df["District"].value_counts()
+    print(district_count)
+    print(district_count.idxmax())
+
+    River_count = df["River"].value_counts()
+    print(River_count)
+    print(River_count.idxmax())
+
+    recent_data = df[df["Last_Date"] > "2018-01-01"]
+    print(recent_data["District"].value_counts())             #last 5 yr data
+
+    khulna_data = df[df["District"] == "Khulna"]
+    print(khulna_data)
+    Satkhira_data = df[df["District"] == "Satkhira"]
+    print(Satkhira_data)
+    Bagerhat_data = df[df["District"] == "Bagerhat"]
+    print(Bagerhat_data)
+
+    district_group = df.groupby("District").size()
+    print(district_group)
+    river_group = df.groupby("River").size()
+    print(river_group)
+
+
+    print(df.isnull().sum())                              #null data check
+
+    
+
+    district_count = df["District"].value_counts()        #chart
+
+    district_count.plot(kind="bar")
+
+    plt.title("Number of Stations by District")
+    plt.xlabel("District")
+    plt.ylabel("Count")
+    plt.show()
+
+
+
+    top_river = df["River"].value_counts().head(5)
+    top_river.plot(kind="bar")
+    plt.title("Top 5 Rivers by Monitoring")
+    plt.show()
+    
+
+except Exception as e:
+    print("Error:", e)
+    
+    
